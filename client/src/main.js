de034b77e0ce9deab3ec9d087281ac2a302d188b
FlorianMMI <florian.bounissou@etu.unlim.fr>
2024-12-16 11:11:45 +0100

    Fin du guide start --> Itération 2

--- client/src/main.js
+++ client/src/main.js
@@ -12,17 +12,68 @@ C.init = async function(){
     console.log(Lycees.getAll());
 }
 
+
+
 let V = {
     header: document.querySelector("#header")
 };
 
 V.init = function(){
     V.renderHeader();
+    V.loadmap();
+    V.marker();
+    V.circle();
+    V.polygon();
+    V.popup();
 }
 
 V.renderHeader= function(){
     V.header.innerHTML = HeaderView.render();
 }
+
+let map;
+
+V.loadmap = function(){
+    map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(map);
+}
+
+V.marker = function(){
+    var marker = L.marker([51.5, -0.09]).addTo(map);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+}
+
+V.circle = function(){
+    var circle = L.circle([51.508, -0.11], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(map);
+    circle.bindPopup("I am a circle.");
+}
+
+V.polygon = function(){
+    var polygon = L.polygon([
+        [51.509, -0.08],
+        [51.503, -0.06],
+        [51.51, -0.047]
+    ]).addTo(map);
+    polygon.bindPopup("I am a polygon.");
+}
+
+V.popup = function(){
+    var popup = L.popup()
+        .setLatLng([51.5, -0.09])
+        .setContent("I am a standalone popup.")
+        .openOn(map);
+}
+
+
+
+
 
 
 C.init();
