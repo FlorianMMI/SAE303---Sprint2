1274295bb6e1ebd5bee3d0f7180303cb441c39cf
FlorianMMI <florian.bounissou@etu.unlim.fr>
2024-12-16 11:28:46 +0100

    itération 3, centrement de la carte sur Limoges + Reflexion autour du modèle MCV

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,10 @@
 import { HeaderView } from "./ui/header/index.js";
 import { Candidats } from "./data/data-candidats.js";
 import { Lycees } from "./data/data-lycees.js";
+
 import './index.css';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 
 let C = {};
@@ -21,10 +24,6 @@ let V = {
 V.init = function(){
     V.renderHeader();
     V.loadmap();
-    V.marker();
-    V.circle();
-    V.polygon();
-    V.popup();
 }
 
 V.renderHeader= function(){
@@ -34,42 +33,19 @@ V.renderHeader= function(){
 let map;
 
 V.loadmap = function(){
-    map = L.map('map').setView([51.505, -0.09], 13);
+    map = L.map('map').setView([45.838643, 1.261065], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
     }).addTo(map);
-}
+} // la map appartient au modèle de la vue car elle est l'affichage de la vue
 
-V.marker = function(){
-    var marker = L.marker([51.5, -0.09]).addTo(map);
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-}
 
-V.circle = function(){
-    var circle = L.circle([51.508, -0.11], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-    }).addTo(map);
-    circle.bindPopup("I am a circle.");
-}
 
-V.polygon = function(){
-    var polygon = L.polygon([
-        [51.509, -0.08],
-        [51.503, -0.06],
-        [51.51, -0.047]
-    ]).addTo(map);
-    polygon.bindPopup("I am a polygon.");
-}
 
-V.popup = function(){
-    var popup = L.popup()
-        .setLatLng([51.5, -0.09])
-        .setContent("I am a standalone popup.")
-        .openOn(map);
-}
+
+
+
+
 
 
 
