cdec8dfe6f30c5d88450e9e5f577646c1cd7714e
FlorianMMI <florian.bounissou@etu.unlim.fr>
2024-12-17 15:20:26 +0100

    Ajout de la fonction de recherche binaire pour optimiser la récupération des lycées et nettoyage du code

--- client/src/data/data-lycees.js
+++ client/src/data/data-lycees.js
@@ -9,10 +9,39 @@ Lycees.getAll = function(){
     return data;
 }
 
+Lycees.binarySearch = function(UAI) {
+    let left = 0;
+    let right = data.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (data[mid].numero_uai === UAI) {
+            return data[mid];
+        } else if (data[mid].numero_uai < UAI) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return null;
+}
+
+
 Lycees.getLyceecandidat = function(){
     data.shift();
+    
     let candidats = Candidats.getAll();
+    
+    let compare = function(a, b){
+        if (a.numero_uai < b.numero_uai){
+            return -1;
+        }
+        if (a.numero_uai > b.numero_uai){
+            return 1;
+        }
+    }
+    data.sort(compare);
 
+    console.log(data);
     for (let Candidat of candidats){
 
         let UAI;
@@ -20,13 +49,13 @@ Lycees.getLyceecandidat = function(){
         for (let annee of Candidat.Scolarite){
             if (annee.UAIEtablissementorigine){
                 UAI = annee.UAIEtablissementorigine;
-                break
+                break;
             }
             nbannnecesure++;
         }
 
 
-            let lycee = data.find(lycee => lycee.numero_uai === UAI);
+            let lycee = Lycees.binarySearch(UAI);
             if (lycee){
                 if (!lycee.candidats){
                     lycee.candidats = [];
@@ -40,8 +69,5 @@ Lycees.getLyceecandidat = function(){
     return data;
 }
 
-Lycees.gettri = function(){
-    return data.sort((a, b) => a.numero_UAI - b.numero_UAI);
-}
 
 export { Lycees };
